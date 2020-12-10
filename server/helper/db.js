dd8b3f921e21d0428dac30f038224d107249aad9
Arpit Bhardwaj <bhardwajarpit24@gmail.com>
2020-12-10 20:46:44 +0530

    Changed DB

--- server/helper/db.js
+++ server/helper/db.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-module.exports=()=>{
-	mongoose.set('useCreateIndex', true);
-    mongoose.connect('mongodb://emre:deneme123@ds117200.mlab.com:17200/deneme',{ useNewUrlParser: true });
-    //mongoose.connect('mongodb://localhost:27017');
-    mongoose.connection.on('open',()=>{
-        console.log('Connection OK');
-    })
-    mongoose.connection.on('error',(err)=>{
-        console.log('Connection Fail',err);
-    })
-}
+module.exports = () => {
+  mongoose.set("useCreateIndex", true);
+  mongoose.connect(
+    "mongodb+srv://arpit:admin@123@cluster0.qlrln.mongodb.net/Cluster0?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  );
+  //mongoose.connect('mongodb://localhost:27017');
+  mongoose.connection.on("open", () => {
+    console.log("Connection OK");
+  });
+  mongoose.connection.on("error", (err) => {
+    console.log("Connection Fail", err);
+  });
+};
